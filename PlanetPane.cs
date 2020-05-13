1ef7dbb7337e9b007843f592f6112c301a87091e
Josh Stratton <strattonbrazil@gmail.com>
2020-05-12 20:47:20 -0700

    ditto

---
+++ PlanetPane.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+
+namespace forgotten.Desktop
+{
+    public class PlanetPane : Pane
+    {
+        public PlanetPane(Planet planet)
+        {
+            Console.WriteLine(planet.Name);
+        }
+
+        public override void Draw(ForgottenGame game, Vector2 targetSize)
+        {
+            //throw new NotImplementedException();
+        }
+
+        public override void Update(Vector2 targetSize, GameTime gameTime)
+        {
+            //throw new NotImplementedException();
+        }
+    }
+}
