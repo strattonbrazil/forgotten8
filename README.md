2222d184c7d4871b20eeb37b7ca68875e9bb3509
Josh Stratton <strattonbrazil@gmail.com>
2020-06-07 10:51:54 -0700

    got some lasers firing

--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 * move "trader" references to "merchant"
 * add anti-aliasing
 * debug why random seed doesn't work as expected
+* make drawn lines centered on rotation point
+* fix starting tail on lasers
 
 === ImageMagick reference
 convert in.gif -layers Coalesce -resize 980x725 -layers Optimize out.gif
--- src/ScreenUtils.cs
+++ src/ScreenUtils.cs
@@ -26,8 +26,16 @@ namespace forgotten.Desktop
 
         public Vector2 WorldToScreen(Vector2 w)
         {
-            return new Vector2((screenW - padding * 2) * ((invAspectRatio * w.X + (worldH * 0.5f)) / worldH) + padding,
-                               (screenH - padding * 2) * ((w.Y + (worldH * 0.5f)) / worldH) + padding);
+            return new Vector2((screenW - padding * 2) * ((invAspectRatio * w.X + worldH * 0.5f) / worldH) + padding,
+                               (screenH - padding * 2) * ((w.Y + worldH * 0.5f) / worldH) + padding);
+        }
+
+        public Vector2 ScreenToWorld(Point screenP)
+        {
+            // TODO: probably can refactor this to be cleaner, but seems to work
+            float worldX = ((((screenP.X - padding) * worldH) / (screenW - padding * 2)) - worldH * 0.5f) * aspectRatio;
+            float worldY = (screenP.Y / (screenH - padding * 2) - padding) * worldH - worldH * 0.5f;
+            return new Vector2(worldX, worldY);
         }
     }
 }
